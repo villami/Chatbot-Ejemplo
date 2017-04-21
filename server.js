de24cf81a205d40a5d71c43af07cf40e3945c160
Josef Grenz <hjosef@mx1.ibm.com>
2017-04-21 12:36:26 -0500

    added debug message

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ http.createServer(function (request, response) {
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
+   console.log('Recived request...');
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
