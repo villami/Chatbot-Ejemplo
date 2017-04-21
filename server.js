7d42f3ed47c568a6d432ab6663790d3ccedf583f
Josef Grenz <hjosef@mx1.ibm.com>
2017-04-21 09:56:22 -0500

    added hello world script

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require("http");
+const PORT = process.env.port || 3000;
+http.createServer(function (request, response) {
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(PORT);
+
+// Console will print the message
+console.log('Server running at port:'+PORT);
